492b471638925499621a223d868ef6c9129012c2
kons16 <ks1216r@gmail.com>
2020-11-22 19:42:07 +0900

    文字色と文字を画像として表示

--- main.go
+++ main.go
@@ -3,20 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
+	"unicode/utf8"
 
 	"github.com/fogleman/gg"
 )
 
 func main() {
-	// rgbs := [][]int{
-	// 	{236, 113, 161},
-	// 	{0, 187, 0},
-	// 	{27, 170, 198},
-	// 	{5, 201, 139},
-	// 	{231, 159, 12},
-	// }
+	rgbs := [][]int{
+		{236, 113, 161},
+		{0, 187, 0},
+		{27, 170, 198},
+		{5, 201, 139},
+		{231, 159, 12},
+	}
 
 	file, err := os.Open("data/test.txt")
 	if err != nil {
@@ -24,26 +27,36 @@ func main() {
 	}
 	defer file.Close()
 
+	size := 128
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		moji := strings.Split(line, " ")[0]
-		//pngName := strings.Split(line, " ")[1]
+		pngName := strings.Split(line, " ")[1]
 
 		lineMojis := strings.Split(moji, "/")
-		lineCnt := len(lineMojis)
-		fmt.Println(lineCnt)
+		lineCnt := len(lineMojis)                   // 行数
+		cnt := utf8.RuneCountInString(lineMojis[0]) // 1行にある文字数
 
-		dc := gg.NewContext(128, 128)
-		dc.SetRGB(1, 1, 1)
+		dc := gg.NewContext(size, size)
 
-		for _, str := range lineMojis {
-			fmt.Println(str)
-			dc.DrawStringAnchored(str, float64(size*len([]rune(message))/2), float64(size/2), 0.5, 0.5)
+		// 文字色を乱数で選択
+		rand.Seed(time.Now().UnixNano())
+		n := rand.Intn(5)
+
+		dc.SetRGB(float64(rgbs[n][0]), float64(rgbs[n][1]), float64(rgbs[n][2]))
+
+		if err := dc.LoadFontFace("rounded-mplus-1c-black.ttf", float64(size/cnt)); err != nil {
+			fmt.Println("load font error")
 		}
 
-		// dc.SavePNG("out/" + pngName + ".png")
+		for i := 0; i < lineCnt; i++ {
+			strImg := lineMojis[i]
+			dc.DrawStringAnchored(strImg, float64(size), float64((size/cnt)*(i+1)), 1, 0)
+		}
 
+		dc.SavePNG("out/" + pngName + ".png")
 	}
 
 	if err = scanner.Err(); err != nil {
